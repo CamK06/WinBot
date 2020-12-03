2eff3c2753c60c1307a76759c20aa40b9a8dbe50
Starman0620 <CamdenK2006@gmail.com>
2020-12-02 21:19:01 -0500

    Added restart command

---
+++ WinWorldBot/Commands/Owner/RestartCommand.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+
+using WinWorldBot.Utils;
+
+namespace WinWorldBot.Commands
+{
+    public class RestartCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("restart")]
+        [Summary("Restart the bot|")]
+        [Priority(Category.Owner)]
+        private async Task Kill()
+        {
+            if(Context.Message.Author.Id != Globals.StarID) return;
+            await ReplyAsync("Restarting...");
+            Log.Write("Restart triggered by command.");
+            "systemctl restart WWBot".Bash();
+        }
+    }
+}
