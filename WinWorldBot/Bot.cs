b42bb927ccda3055bd3d238665e3cee542ed74ce
Starman0620 <CamdenK2006@gmail.com>
2020-12-24 12:19:06 -0500

    Added exception for Starman in the #media command restrictions

--- WinWorldBot/Bot.cs
+++ WinWorldBot/Bot.cs
@@ -105,7 +105,9 @@ namespace WinWorldBot
 
 #if RELEASE
             // This is a messy fix to allow commands outside of media but oh well
+            if(arg.Author.Id != 363850072309497876) {
             if(arg.Channel.Id != 563206142755471381 && !arg.Content.ToLower().Contains("ev") && !arg.Content.ToLower().Contains("wiki") && !arg.Content.ToLower().Contains("mcinfo") && arg.Channel.Id != 474350814387765250) return;
+            }
 #endif      // Basic setup for handling the command
             string messageLower = arg.Content.ToLower();
             SocketUserMessage message = arg as SocketUserMessage;
