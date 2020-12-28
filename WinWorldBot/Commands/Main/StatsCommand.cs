9d2758584a8e76268cbf8f815e05be9dfcd3b450
Starman0620 <CamdenK2006@gmail.com>
2020-12-28 16:26:09 -0500

    Added Yuds questions to stats

--- WinWorldBot/Commands/Main/StatsCommand.cs
+++ WinWorldBot/Commands/Main/StatsCommand.cs
@@ -21,10 +21,12 @@ namespace WinWorldBot.Commands
             string nortonS = File.ReadAllText("nortons");
             string ohS = File.ReadAllText("oh");
             string okS = File.ReadAllText("ok");
+            string question = File.ReadAllText("?");
             var uptime = DateTime.Now.Subtract(Bot.startTime);
             int.TryParse(nortonS, out int norton);
             int.TryParse(ohS, out int oh);
             int.TryParse(okS, out int okay);
+            int.TryParse(question, out int questions);
 
             // Build the embed
             EmbedBuilder eb = new EmbedBuilder();
@@ -34,8 +36,9 @@ namespace WinWorldBot.Commands
             eb.AddField("Member Count", Context.Guild.MemberCount, true);
             eb.AddField("Norton Count", norton, true);
             if(Context.Channel.Id == 563206142755471381) {
-                eb.AddField("Oh Count", oh, true);
-                eb.AddField("Ok Count", okay, true);
+                //eb.AddField("Oh Count", oh, true);
+                //eb.AddField("Ok Count", okay, true);
+                eb.AddField("? Count", questions, true);
             }
             //eb.AddField("Uptime", $"{uptime.Days}:{uptime.Hours}:{uptime.Minutes}:{uptime.Seconds}", true);
             eb.AddField("Uptime", TimeSpanHumanizeExtensions.Humanize(uptime), true);
