5d984940e6ba697fc5a7f289369fb7ff7503099d
Starman0620 <CamdenK2006@gmail.com>
2020-12-31 14:33:12 -0500

    Fixed typo in the rate rigging table

--- WinWorldBot/Commands/Fun/RateCommand.cs
+++ WinWorldBot/Commands/Fun/RateCommand.cs
@@ -34,7 +34,7 @@ namespace WinWorldBot.Commands
             { "windows 7", 11 }, { "windows 2000", 11 }, { "duff", 486 },
             { "windows10", -99 }, { "windows 98", 98 }, { "windows98", 98 },
             { "microsoft windows 10", -99}, { "microsoft windows10", -99 },
-            { "microsoftwindows10", - 99 }
+            { "microsoftwindows10", -99 }
         };
     }
 }
