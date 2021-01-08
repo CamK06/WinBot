b06f621c961276002f8caeb721362f924f9335f7
Starman0620 <CamdenK2006@gmail.com>
2021-01-08 12:58:13 -0500

    Added cookie command

---
+++ WinWorldBot/Commands/Fun/CookieCommand.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+
+namespace WinWorldBot.Commands
+{
+    public class CookieCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("cookie")]
+        [Summary("Give someone a cookie|[User]")]
+        [Priority(Category.Fun)]
+        private async Task Cookie(SocketGuildUser user = null)
+        {
+            if(user == null) 
+            {
+                await ReplyAsync("You must provide a user to give a cookie to!");
+                return;
+            }
+
+            await ReplyAsync($"{user.Mention}: {Context.Message.Author.Username} gave you a cookie! 🍪");
+        }
+    }
+}
