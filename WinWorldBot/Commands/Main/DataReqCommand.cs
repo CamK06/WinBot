00fae45e2b1791a4d0d1943ceae3056439442b94
Starman0620 <CamdenK2006@gmail.com>
2021-01-21 15:42:51 -0500

    Fixed file formatting issue

--- WinWorldBot/Commands/Main/DataReqCommand.cs
+++ WinWorldBot/Commands/Main/DataReqCommand.cs
@@ -19,7 +19,7 @@ namespace WinWorldBot.Commands
         private async Task RequestData()
         {
             // Skip creating the json and all file checks if the data exists already
-            if(File.Exists($"DataRequests/{Context.Message.Author.Username}-{DateTime.Now.ToShortDateString()}.json")) goto send;
+            if(File.Exists($"DataRequests/{Context.Message.Author.Username}-{DateTime.Now.ToShortDateString().Replace("/", "-")}.json")) goto send;
 
             // Get the data
             await ReplyAsync("Converting data to Json...");
@@ -27,11 +27,11 @@ namespace WinWorldBot.Commands
 
             // Write the file
             if(!Directory.Exists("DataRequests")) Directory.CreateDirectory("DataRequests");
-            File.WriteAllText($"DataRequests/{Context.Message.Author.Username}-{DateTime.Now.ToShortDateString()}.json", json);
+            File.WriteAllText($"DataRequests/{Context.Message.Author.Username}-{DateTime.Now.ToShortDateString().Replace("/", "-")}.json", json);
 
             // Verify the file size
             send:
-            if(File.ReadAllBytes($"DataRequests/{Context.Message.Author.Username}-{DateTime.Now.ToShortDateString()}.json").Length > 7500000)
+            if(File.ReadAllBytes($"DataRequests/{Context.Message.Author.Username}-{DateTime.Now.ToShortDateString().Replace("/", "-")}.json").Length > 7500000)
             {
                 await ReplyAsync("Your data was unable to be sent due to the absurdly small file size limit from Discord. Starman has been notified and should manually provide your data.");
                 await Context.Guild.GetUser(Globals.StarID).SendMessageAsync($"Oi, {Context.Message.Author}'s data could not be delivered thanks to Discord. The file name is ``DataRequests/{Context.Message.Author.Username}-{DateTime.Now.ToShortDateString()}.json``. GIVE IT TO THEM YA FOOKIN WANKER");
@@ -39,7 +39,7 @@ namespace WinWorldBot.Commands
             }
 
             // Send the data
-            await Context.Message.Author.SendFileAsync($"DataRequests/{Context.Message.Author.Username}-{DateTime.Now.ToShortDateString()}.json", "Here's your data!");
+            await Context.Message.Author.SendFileAsync($"DataRequests/{Context.Message.Author.Username}-{DateTime.Now.ToShortDateString().Replace("/", "-")}.json", "Here's your data!");
             await ReplyAsync("Your data has been sent! Check your DMs.");
         }
     }
