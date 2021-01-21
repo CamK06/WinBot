8b66d57df2c734d650fb33c4862ad47278ca0f75
Starman0620 <CamdenK2006@gmail.com>
2021-01-21 11:07:50 -0500

    Replaced json user data with binary

--- WinWorldBot/Data/UserData.cs
+++ WinWorldBot/Data/UserData.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
 
 using WinWorldBot.Utils;
 
@@ -14,6 +15,7 @@ namespace WinWorldBot.Data
     class UserData
     {
         public static List<User> Users = new List<User>();
+        private static BinaryFormatter formatter = new BinaryFormatter();
 
         public static User GetUser(SocketUser user)
         {
@@ -38,6 +40,31 @@ namespace WinWorldBot.Data
 
         public static void LoadData()
         {
+            if (File.Exists("UserData.bin"))
+            {
+                FileStream file = new FileStream("UserData.bin", FileMode.OpenOrCreate);
+                Users = (List<User>)formatter.Deserialize(file);
+                file.Close();
+                Log.Write("Loaded user data");
+            }
+            else
+            {
+                Users = new List<User>();
+                SaveData();
+                Log.Write("Created user data file");
+            }
+        }
+
+        public static void SaveData()
+        {
+            FileStream file = new FileStream("UserData.bin", FileMode.OpenOrCreate);
+            formatter.Serialize(file, Users);
+            file.Close();
+        }
+
+        /* Outdated JSON code, this is left here just in case it's needed for some reason in the future
+        public static void LoadData()
+        {
             if (File.Exists("UserData.json"))
             {
                 string json = File.ReadAllText("UserData.json");
@@ -56,5 +83,6 @@ namespace WinWorldBot.Data
         {
             File.WriteAllText("UserData.json", JsonConvert.SerializeObject(Users, Formatting.Indented));
         }
+        */
     }
 }
