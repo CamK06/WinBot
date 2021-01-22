a12a35d3a8e6ba3fa4e171a55974fa56ef212750
Starman0620 <CamdenK2006@gmail.com>
2021-01-22 11:02:47 -0500

    Added status to whois

--- WinWorldBot/Commands/Main/WhoIsCommand.cs
+++ WinWorldBot/Commands/Main/WhoIsCommand.cs
@@ -29,6 +29,14 @@ namespace WinWorldBot.Commands
                 Embed.AddField("**Nickname**", user.Nickname);
             if(user.CreatedAt != null) Embed.AddField("**Created On**", MiscUtil.FormatDate(user.CreatedAt), true);
             if(user.JoinedAt.HasValue) Embed.AddField("**Joined On**", MiscUtil.FormatDate(user.JoinedAt.Value), true);
+            
+            // This is kinda messy but oh well, idc
+            if(user.Activity.Type == ActivityType.Listening) Embed.AddField("**Listening**", user.Activity.Details, true);
+            else if(user.Activity.Type == ActivityType.Playing) Embed.AddField("**Playing**", user.Activity.Details, true);
+            else if(user.Activity.Type == ActivityType.Watching) Embed.AddField("**Watching**", user.Activity.Details, true);
+            else if(user.Activity.Type == ActivityType.Streaming) Embed.AddField("**Streaming**", user.Activity.Details, true);
+            else if(user.Activity.Type == ActivityType.CustomStatus) Embed.AddField("**Status**", user.Activity.Details, true);
+
             //Embed.AddField("**Subscription**", "Buy our shitty premium to instantly regret our 'perks'!", true);
             
             await ReplyAsync("", false, Embed.Build());
