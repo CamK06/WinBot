5305eafe50c46f3bc3d85763a643a0a9cd742e85
Starman0620 <CamdenK2006@gmail.com>
2021-01-22 11:06:24 -0500

    Fixed whois embed

--- WinWorldBot/Commands/Main/WhoIsCommand.cs
+++ WinWorldBot/Commands/Main/WhoIsCommand.cs
@@ -16,32 +16,37 @@ namespace WinWorldBot.Commands
         [Priority(Category.Main)]
         private async Task WhoIs(SocketGuildUser user)
         {
-            try {
-            EmbedBuilder Embed = new EmbedBuilder();
-            Embed.WithColor(Bot.config.embedColour);
-            if(user != null) Embed.WithAuthor(user);
-            if(user.GetAvatarUrl() != null)
-                Embed.WithThumbnailUrl(user.GetAvatarUrl());
-            else
-                Embed.WithThumbnailUrl(user.GetDefaultAvatarUrl());
-            Embed.AddField("**ID**", user.Id, false);
-            if(!string.IsNullOrWhiteSpace(user.Nickname))
-                Embed.AddField("**Nickname**", user.Nickname);
-            if(user.CreatedAt != null) Embed.AddField("**Created On**", MiscUtil.FormatDate(user.CreatedAt), true);
-            if(user.JoinedAt.HasValue) Embed.AddField("**Joined On**", MiscUtil.FormatDate(user.JoinedAt.Value), true);
-            
-            // This is kinda messy but oh well, idc
-            if(user.Activity.Type == ActivityType.Listening) Embed.AddField("**Listening**", user.Activity.Details, true);
-            else if(user.Activity.Type == ActivityType.Playing) Embed.AddField("**Playing**", user.Activity.Details, true);
-            else if(user.Activity.Type == ActivityType.Watching) Embed.AddField("**Watching**", user.Activity.Details, true);
-            else if(user.Activity.Type == ActivityType.Streaming) Embed.AddField("**Streaming**", user.Activity.Details, true);
-            else if(user.Activity.Type == ActivityType.CustomStatus) Embed.AddField("**Status**", user.Activity.Details, true);
+            try
+            {
+                EmbedBuilder Embed = new EmbedBuilder();
+                Embed.WithColor(Bot.config.embedColour);
+                if (user != null) Embed.WithAuthor(user);
+                if (user.GetAvatarUrl() != null)
+                    Embed.WithThumbnailUrl(user.GetAvatarUrl());
+                else
+                    Embed.WithThumbnailUrl(user.GetDefaultAvatarUrl());
+                Embed.AddField("**ID**", user.Id, false);
+                if (!string.IsNullOrWhiteSpace(user.Nickname))
+                    Embed.AddField("**Nickname**", user.Nickname);
+                if (user.CreatedAt != null) Embed.AddField("**Created On**", MiscUtil.FormatDate(user.CreatedAt), true);
+                if (user.JoinedAt.HasValue) Embed.AddField("**Joined On**", MiscUtil.FormatDate(user.JoinedAt.Value), true);
 
-            //Embed.AddField("**Subscription**", "Buy our shitty premium to instantly regret our 'perks'!", true);
-            
-            await ReplyAsync("", false, Embed.Build());
+                // This is kinda messy but oh well, idc
+                if (user.Activity != null)
+                {
+                    if (user.Activity.Type == ActivityType.Listening) Embed.AddField("**Listening**", user.Activity.ToString(), true);
+                    else if (user.Activity.Type == ActivityType.Playing) Embed.AddField("**Playing**", user.Activity.ToString(), true);
+                    else if (user.Activity.Type == ActivityType.Watching) Embed.AddField("**Watching**", user.Activity.ToString(), true);
+                    else if (user.Activity.Type == ActivityType.Streaming) Embed.AddField("**Streaming**", user.Activity.ToString(), true);
+                    else if (user.Activity.Type == ActivityType.CustomStatus) Embed.AddField("**Status**", user.Activity.ToString(), true);
+                }
+
+                //Embed.AddField("**Subscription**", "Buy our shitty premium to instantly regret our 'perks'!", true);
+
+                await ReplyAsync("", false, Embed.Build());
             }
-            catch(Exception ex) {
+            catch (Exception ex)
+            {
                 await ReplyAsync("Error: " + ex.Message + "\nStack Trace:" + ex.StackTrace);
             }
         }
