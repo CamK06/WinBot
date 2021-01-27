a28dc2e02118398be5f6d19d143ab94b3055f322
Starman0620 <CamdenK2006@gmail.com>
2021-01-26 19:06:38 -0500

    Fixed bed command

--- WinWorldBot/Commands/Fun/BedCommand.cs
+++ WinWorldBot/Commands/Fun/BedCommand.cs
@@ -14,15 +14,10 @@ namespace WinWorldBot.Commands
         [Priority(Category.Fun)]
         private async Task Bed(SocketGuildUser user)
         {
-            int val = new Random().Next(0, 1);
-
             // Create and send the embed
             var eb = new EmbedBuilder();
             eb.WithColor(Bot.config.embedColour);
-            if(val == 0)
-                eb.WithImageUrl("https://cdn.discordapp.com/attachments/474350814387765250/801215437668089896/77e66a99419397dcd2316cdad65a1010.png");
-            else if(val == 1)
-                eb.WithImageUrl("https://cdn.discordapp.com/attachments/563206142755471381/801593276271820861/parzgotobed.jpg");
+            eb.WithImageUrl("https://cdn.discordapp.com/attachments/563206142755471381/801593276271820861/parzgotobed.jpg");
             await ReplyAsync($"GO TO BED, {user.Mention}", false, eb.Build());
         }
     }
