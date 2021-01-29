f9156255cc73695320a28e3039a72dfc463a555b
Starman0620 <CamdenK2006@gmail.com>
2021-01-29 11:02:26 -0500

    Removed user counting from the count command

--- WinWorldBot/Commands/Main/CountCommand.cs
+++ WinWorldBot/Commands/Main/CountCommand.cs
@@ -14,29 +14,20 @@ namespace WinWorldBot.Commands
     public class CountCommand : ModuleBase<SocketCommandContext>
     {
         [Command("count")]
-        [Summary("Determine how many times something has been said|\"[Word or Phrase]\" [User]")]
+        [Summary("Determine how many times something has been said|[Word or Phrase]")]
         [Priority(Category.Main)]
-        private async Task Count(string text, SocketGuildUser user = null)
+        private async Task Count(string text)
         {
             await Context.Channel.TriggerTypingAsync();
-            
+
             int count = 0;
             EmbedBuilder eb = new EmbedBuilder();
             eb.WithColor(Color.Gold);
 
-            if(user != null)
-            {
-                foreach(UserMessage msg in UserData.GetUser(user).Messages)
-                    if(msg.Content != null && msg.Content.ToLower().Contains(text.ToLower())) count++;
-                eb.WithTitle($"{user.Username} has said \"{text}\" {count} times since {UserData.GetUser(user).StartedLogging.ToShortDateString()}");
-            }
-            else
-            {
-                foreach(User member in UserData.Users)
-                    foreach(UserMessage msg in member.Messages)
-                        if(msg.Content != null && msg.Content.ToLower().Contains(text.ToLower())) count++;
-                eb.WithTitle($"\"{text}\" has been said {count} times since {new DateTime(2021, 1, 10).ToShortDateString()}");
-            }
+            foreach (User member in UserData.Users)
+                foreach (UserMessage msg in member.Messages)
+                    if (msg.Content != null && msg.Content.ToLower().Contains(text.ToLower())) count++;
+            eb.WithTitle($"\"{text}\" has been said {count} times since {new DateTime(2021, 1, 10).ToShortDateString()}");
 
             await ReplyAsync("", false, eb.Build());
         }
