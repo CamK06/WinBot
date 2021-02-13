97354c2d0aba9ca382b316d493687f4e3a7bcc49
Starman0620 <CamdenK2006@gmail.com>
2021-02-13 00:06:25 -0500

    Slightly improved help command comments

--- WinBot/Source/Commands/Main/HelpCommand.cs
+++ WinBot/Source/Commands/Main/HelpCommand.cs
@@ -26,15 +26,18 @@ namespace WinBot.Commands
 		static string GetCommands(int category)
 		{
 			string finalString = "";
+			// Loop over every command
 			for(int i = 0; i < Bot.commands.Commands.Count(); i++) 
 			{
 				CommandInfo command = Bot.commands.Commands.ToArray()[i];
+				// If the command is in the category we're looking for
 				if(command.Priority == category)
 				{
 					if(!string.IsNullOrWhiteSpace(finalString)) finalString += $" | `{command.Name}`";
 					else finalString = $"`{command.Name}`";
 				}
 			}
+
 			return finalString;
 		}
 	}
