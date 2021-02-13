7e50d62b7bf3952369597dc4b54cde5adc2b8afb
Starman0620 <CamdenK2006@gmail.com>
2021-02-12 23:18:32 -0500

    Added new project files

---
+++ WinBot/Source/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WinBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
