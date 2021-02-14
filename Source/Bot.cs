2e0c6d1519a9b241bc4daf76944e32afc7a8102f
Starman0620 <CamdenK2006@gmail.com>
2021-02-13 23:17:14 -0500

    Added Yuds counter

--- Source/Bot.cs
+++ Source/Bot.cs
@@ -59,47 +59,64 @@ namespace WinBot
 			await commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
 
 			// Set up events
-			client.Log += (LogMessage message) => 
+			client.Log += (LogMessage message) =>
 			{
 				Log.Write(message.Message);
 				return Task.CompletedTask;
 			};
 			client.MessageReceived += HandleCommandAsync;
-			
+
 			// Start the bot
 			await client.LoginAsync(TokenType.Bot, config.token);
 			await client.StartAsync();
-			if(!string.IsNullOrWhiteSpace(config.activity))
+			if (!string.IsNullOrWhiteSpace(config.activity))
 				await client.SetGameAsync(config.activity);
-			
-			
+
+
 			await Task.Delay(-1);
 		}
 
 		private async Task HandleCommandAsync(SocketMessage arg)
 		{
+			/*	
+			 Yuds counter
+			 Tbh I'd rather not have this but I mean, it annoys Yuds so it should stay.
+			 and I *would* make a better implementation but I cba to convert the existing data and whatnot.
+			 Also, this has been reverted to just count question marks because the frequency of questions has lowered
+			 and most have question marks anyways. Plus I don't feel like adding or rewriting the more complicated detection.
+			*/
+			if (arg.Author.Id == 469275318079848459 && arg.Content.ToLower().Contains("?"))
+			{
+				if(!File.Exists("?"))
+					File.WriteAllText("?", "0");
+				string text = File.ReadAllText("?");
+				int.TryParse(text, out int question);
+				question++;
+				File.WriteAllText("?", question.ToString());
+			}
+
 			// Tell people to fuck off for pinging Duff
-			if(arg.Content.ToLower().Contains("283982771997638658"))
+			if (arg.Content.ToLower().Contains("283982771997638658"))
 				await arg.Channel.SendMessageAsync("https://tenor.com/view/oh-fuck-off-go-away-just-go-leave-me-alone-spicy-wings-gif-14523970");
 
 			// Basic setup
 			string loMsg = arg.Content.ToLower();
 			SocketUserMessage message = (SocketUserMessage)arg;
-			if(message == null || message.Author.IsBot && !message.Author.IsWebhook) return;
+			if (message == null || message.Author.IsBot && !message.Author.IsWebhook) return;
 			int argPos = 0;
 
 			// Execute the command
-			if(message.HasStringPrefix(config.prefix, ref argPos))
+			if (message.HasStringPrefix(config.prefix, ref argPos))
 			{
 				SocketCommandContext ctx = new SocketCommandContext(client, message);
 				IResult result = await commands.ExecuteAsync(ctx, argPos, services);
-				
-				if(!result.IsSuccess && !result.ErrorReason.ToLower().Contains("unknown command"))
+
+				if (!result.IsSuccess && !result.ErrorReason.ToLower().Contains("unknown command"))
 				{
 					Log.Write(result.ErrorReason);
 					await message.Channel.SendMessageAsync($"⚠️ Error: {result.ErrorReason} ⚠️\nConsult Starman or the help page for the command you executed. (.help [command])");
 				}
-				else if(result.IsSuccess)
+				else if (result.IsSuccess)
 					Log.Write($"{arg.Author} executed command: {arg.Content}", false);
 			}
 		}
