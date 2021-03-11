9b0f23a3e6237c2723c980cecb41c072a92be039
Starman0620 <CamdenK2006@gmail.com>
2021-03-11 10:49:45 -0500

    Changed owner category to staff

--- Source/Commands/Main/HelpCommand.cs
+++ Source/Commands/Main/HelpCommand.cs
@@ -22,7 +22,7 @@ namespace WinBot.Commands.Main
 				helpEmbed.WithTitle("WinBot Commands");
 				helpEmbed.AddField("**Main**", GetCommands(Category.Main), false);
 				helpEmbed.AddField("**Fun**", GetCommands(Category.Fun), false);
-				helpEmbed.AddField("**Owner**", GetCommands(Category.Owner), false);
+				helpEmbed.AddField("**Staff**", GetCommands(Category.Owner), false);
 			}
 			else
 			{
