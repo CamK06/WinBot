b3db644678ea0a61baa3ab7e93fd6ef381ff11bb
Starman0620 <CamdenK2006@gmail.com>
2021-03-13 01:16:48 -0500

    Added real GIF support to graph

--- Source/Commands/Fun/GraphCommand.cs
+++ Source/Commands/Fun/GraphCommand.cs
@@ -3,10 +3,13 @@ using System.IO;
 using System.Net;
 using System.Linq;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Threading.Tasks;
 
 using Discord;
 using Discord.Commands;
+
+using AnimatedGif;
 
 namespace WinBot.Commands.Fun
 {
@@ -22,18 +25,18 @@ namespace WinBot.Commands.Fun
 			// Change the image URL to an attachment if one is present
 			if (Context.Message.Attachments.Count > 0)
 				image = Context.Message.Attachments.FirstOrDefault().Url;
-			else if(Emote.TryParse(image, out Emote emote))
+			else if (Emote.TryParse(image, out Emote emote))
 				image = emote.Url;
 
 			// Check filesize
 			client.OpenRead(image);
 			Int64 fileSize = Convert.ToInt64(client.ResponseHeaders["Content-Length"]);
-			if(!client.ResponseHeaders["Content-Type"].Contains("image") || client.ResponseHeaders["Content-Type"].Contains("svg"))
+			if (!client.ResponseHeaders["Content-Type"].Contains("image") || client.ResponseHeaders["Content-Type"].Contains("svg"))
 			{
 				await ReplyAsync("Your file is not a valid image!");
 				return;
 			}
-			if(fileSize > 16777216) // 16MB limit
+			if (fileSize > 16777216) // 16MB limit
 			{
 				await ReplyAsync("Your file must be below 16MB in size!");
 				return;
@@ -42,6 +45,11 @@ namespace WinBot.Commands.Fun
 
 			// Download the image
 			client.DownloadFile(image, $"graph.{extension}");
+			if (extension == "gif")
+			{
+				await CreateGif();
+				return;
+			}
 
 			// Create the image
 			Bitmap chad = new Bitmap(Bitmap.FromFile("chadClean.png"));
@@ -62,5 +70,42 @@ namespace WinBot.Commands.Fun
 			new Point(1440, 385), // Top right
 			new Point(985, 890),  // Bottom left
 		};
+
+		async Task CreateGif()
+		{
+			// Setup
+			Bitmap chad = new Bitmap(Bitmap.FromFile("chadClean.png"));
+			Bitmap[] chads;
+
+			// Read the original gif
+			System.Drawing.Image originalGif = System.Drawing.Image.FromFile("graph.gif");
+			FrameDimension dimension = new FrameDimension(originalGif.FrameDimensionsList[0]);
+			int frameCount = originalGif.GetFrameCount(dimension);
+			if (frameCount > 128) frameCount = 128;
+			chads = new Bitmap[frameCount];
+
+			// Process the original gif
+			if (!Directory.Exists("gifTemp"))
+				Directory.CreateDirectory("gifTemp");
+			for (int i = 0; i < frameCount; i++)
+			{
+				// Create graph for current frame
+				originalGif.SelectActiveFrame(dimension, i);
+				using (Graphics canvas = Graphics.FromImage(chad))
+				{
+					canvas.DrawImage(originalGif, graphPos);
+					canvas.Save();
+				}
+				chads[i] = new Bitmap(chad);
+			}
+
+			// Create the gif
+			using (var gif = AnimatedGif.AnimatedGif.Create("nickelback.gif", 66))
+				for (int i = 0; i < frameCount; i++)
+					await gif.AddFrameAsync(System.Drawing.Image.FromFile($"gifTemp/{i}.png"));
+
+			// Send the gif
+			await Context.Channel.SendFileAsync("nickelback.gif");
+		}
 	}
 }
