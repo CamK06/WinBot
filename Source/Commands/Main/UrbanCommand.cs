27c33b9cccb23339f51a5f920a360406a3962893
Starman0620 <CamdenK2006@gmail.com>
2021-03-14 15:18:59 -0400

    Fixed no example bug in urban command

--- Source/Commands/Main/UrbanCommand.cs
+++ Source/Commands/Main/UrbanCommand.cs
@@ -22,11 +22,14 @@ namespace WinBot.Commands.Main
 			var definition = await api.SearchTermAsync(query);
 
 			// Error checking
-			if (definition.List.Count < 1)
+			bool hasExample = true;
+			if (definition.List.Count < 1 || string.IsNullOrWhiteSpace(definition.List.First().Definition.Truncate(1024)))
 			{
 				await ReplyAsync("Error: There are no results for that query.");
 				return;
 			}
+			else if(string.IsNullOrWhiteSpace(definition.List.First().Example.Truncate(1024)))
+				hasExample = false;
 
 			// Create an embed
 			EmbedBuilder eb = new EmbedBuilder();
@@ -34,7 +37,8 @@ namespace WinBot.Commands.Main
 			eb.WithTitle($"Urban Dictionary: {query}");
 			eb.WithColor(Color.Gold);
 			eb.AddField("Definition", result.Definition.Truncate(1024));
-			eb.AddField("Examples", result.Example.Truncate(1024));
+			if(hasExample)
+				eb.AddField("Examples", result.Example.Truncate(1024));
 			eb.WithUrl(result.Permalink);
 			eb.WithThumbnailUrl("https://reclaimthenet.org/wp-content/uploads/2020/07/urban-dictionary.png");
 			eb.WithFooter("The information above does not represent the views of WinWorld or Starman, obviously :P");
