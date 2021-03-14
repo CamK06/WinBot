e4415f2d49d921fc6076e12a320117d7125a8243
Frankie <frankieraybrown@gmail.com>
2021-03-14 20:08:20 +0000

    Added Rory command

---
+++ Source/Commands/Fun/RoryCommand.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using Discord;
+using Discord.Commands;
+
+namespace WinBot.Commands.Fun
+{
+    public class RoryCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("rory")]
+        [Summary("Gets a random picture of Rory!")]
+        [Priority(Category.Fun)]
+        public async Task Rory()
+        {
+            string json = "";
+            // Grab the json string from the API
+            using (WebClient client = new WebClient())
+                json = client.DownloadString("https://rory.cat/purr");
+            dynamic output = JsonConvert.DeserializeObject(json); // Deserialize the string into a dynamic object
+
+            string roryurl = (string)output.url;
+
+            await ReplyAsync("**Rory ID:** " + (string)output.id + "\n**Link:** " + roryurl);
+            //await ReplyAsync(roryurl);
+        }
+    }
+}
