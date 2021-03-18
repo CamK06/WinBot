e7f6ab853f896e456a411dd8ec5a4c7276e7f6b9
Starman0620 <CamdenK2006@gmail.com>
2021-03-17 23:57:29 -0400

    Improved X ticks on graph

--- Source/Commands/Main/ServerStatsCommand.cs
+++ Source/Commands/Main/ServerStatsCommand.cs
@@ -35,7 +35,6 @@ namespace WinBot.Commands.Main
             double[] commands = new double[reports.Count];
             //double[] userJoin = new double[reports.Count];
             //double[] userLeave = new double[reports.Count];
-            string[] xticks = new string[reports.Count];
             for(int i = 0; i < reports.Count; i++) {
                 messages[i] += reports[i].messagesSent;
                 commands[i] += reports[i].commandsRan;
@@ -45,22 +44,22 @@ namespace WinBot.Commands.Main
 
             double[] ys = new double[reports.Count];
             for(int i = 0; i < reports.Count; i++) {
-                ys[i] = i;
-                xticks[i] = reports[i].dayOfReport.ToShortDateString();
+                ys[i] = reports[i].dayOfReport.ToOADate();
             }
             
             // Plotting
             Plot plt = new Plot(1920, 1080);
             plt.Style(System.Drawing.Color.FromArgb(52, 54, 60), System.Drawing.Color.FromArgb(52, 54, 60), null, System.Drawing.Color.White, System.Drawing.Color.White, System.Drawing.Color.White);
-            plt.XLabel("Day", null, null, null, 25.5f, false);
+            //plt.XLabel("Day", null, null, null, 25.5f, false);
             plt.YLabel("Count", null, null, 25.5f, null, false);
             plt.PlotFillAboveBelow(ys, messages, "Messages", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(100, 119, 183), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(100, 119, 183), fillColorAbove: System.Drawing.Color.FromArgb(100, 119, 183));
             plt.PlotFillAboveBelow(ys, commands, "Command Executions", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(252, 186, 3), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(252, 186, 3), fillColorAbove: System.Drawing.Color.FromArgb(252, 186, 3));
             //plt.PlotFillAboveBelow(ys, userJoin, "Users Joined", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(252, 3, 3), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(252, 3, 3), fillColorAbove: System.Drawing.Color.FromArgb(252, 3, 3));
             //plt.PlotFillAboveBelow(ys, userLeave, "Users Left", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(15, 252, 3), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(15, 252, 3), fillColorAbove: System.Drawing.Color.FromArgb(15, 252, 3));
             plt.TightenLayout(0, true);
-            plt.XTicks(xticks);
+            plt.Ticks(dateTimeX: true, xTickRotation: 75);
             plt.Title("WinWorldPC Statistics", null, null, 45.5f, null, true);
+			plt.Grid(xSpacing: 1, xSpacingDateTimeUnit: ScottPlot.Config.DateTimeUnit.Day);
             plt.Legend(true, null, 30, null, null, System.Drawing.Color.FromArgb(100, 52, 54, 60), null, legendLocation.upperRight, shadowDirection.lowerRight, null, null);
 
             // Save and send
