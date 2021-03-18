fe9a02eee5472d7a6bda345434f5d815362a6e0d
Starman0620 <CamdenK2006@gmail.com>
2021-03-17 23:52:53 -0400

    Removed users joined and left from serverstats

--- Source/Commands/Main/ServerStatsCommand.cs
+++ Source/Commands/Main/ServerStatsCommand.cs
@@ -33,14 +33,14 @@ namespace WinBot.Commands.Main
             // Data parsing
             double[] messages = new double[reports.Count];
             double[] commands = new double[reports.Count];
-            double[] userJoin = new double[reports.Count];
-            double[] userLeave = new double[reports.Count];
+            //double[] userJoin = new double[reports.Count];
+            //double[] userLeave = new double[reports.Count];
             string[] xticks = new string[reports.Count];
             for(int i = 0; i < reports.Count; i++) {
                 messages[i] += reports[i].messagesSent;
                 commands[i] += reports[i].commandsRan;
-                userJoin[i] += reports[i].usersJoined;
-                userLeave[i] += reports[i].usersLeft;
+                //userJoin[i] += reports[i].usersJoined;
+                //userLeave[i] += reports[i].usersLeft;
             }
 
             double[] ys = new double[reports.Count];
@@ -56,8 +56,8 @@ namespace WinBot.Commands.Main
             plt.YLabel("Count", null, null, 25.5f, null, false);
             plt.PlotFillAboveBelow(ys, messages, "Messages", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(100, 119, 183), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(100, 119, 183), fillColorAbove: System.Drawing.Color.FromArgb(100, 119, 183));
             plt.PlotFillAboveBelow(ys, commands, "Command Executions", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(252, 186, 3), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(252, 186, 3), fillColorAbove: System.Drawing.Color.FromArgb(252, 186, 3));
-            plt.PlotFillAboveBelow(ys, userJoin, "Users Joined", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(252, 3, 3), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(252, 3, 3), fillColorAbove: System.Drawing.Color.FromArgb(252, 3, 3));
-            plt.PlotFillAboveBelow(ys, userLeave, "Users Left", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(15, 252, 3), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(15, 252, 3), fillColorAbove: System.Drawing.Color.FromArgb(15, 252, 3));
+            //plt.PlotFillAboveBelow(ys, userJoin, "Users Joined", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(252, 3, 3), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(252, 3, 3), fillColorAbove: System.Drawing.Color.FromArgb(252, 3, 3));
+            //plt.PlotFillAboveBelow(ys, userLeave, "Users Left", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(15, 252, 3), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(15, 252, 3), fillColorAbove: System.Drawing.Color.FromArgb(15, 252, 3));
             plt.TightenLayout(0, true);
             plt.XTicks(xticks);
             plt.Title("WinWorldPC Statistics", null, null, 45.5f, null, true);
