a027ce787cc7b65df0b1a06d770a66749c225cb9
Starman0620 <CamdenK2006@gmail.com>
2021-04-02 21:04:51 -0400

    Added wiki command

---
+++ Source/Commands/Main/WikiCommand.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using System.Threading.Tasks;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+using WinBot.Commands.Attributes;
+
+using WikipediaNet;
+using WikipediaNet.Objects;
+
+namespace WinBot.Commands.Main
+{
+    public class WikiCommand : BaseCommandModule
+    {
+        [Command("wiki")]
+        [Description("Search wikipedia")]
+        [Usage("query")]
+        [Category(Category.Main)]
+        public async Task Wiki(CommandContext Context, [RemainingText] string query)
+        {
+            Wikipedia wiki = new Wikipedia();
+            wiki.Limit = 1;
+            QueryResult results = wiki.Search(query);
+
+            if (results.SearchInfo.TotalHits < 1)
+            {
+                await Context.RespondAsync($"Error: There are no results for that query.");
+                return;
+            }
+
+            await Context.RespondAsync(results.Search.First().Url.ToString().Replace(" ", "_"));
+        }
+    }
+}
