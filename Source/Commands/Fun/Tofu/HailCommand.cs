abdecfdc9fb359dd9eee4bd27d2f0556c286d576
Starman0620 <28871190+Starman0620@users.noreply.github.com>
2021-05-10 16:44:35 -0400

    Added hail command

---
+++ Source/Commands/Fun/Tofu/HailCommand.cs
@@ -0,0 +1,31 @@
+#if TOFU
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+using WinBot.Commands.Attributes;
+
+namespace WinBot.Commands.Fun.Tofu
+{
+    public class HailCommand : BaseCommandModule
+    {
+        [Command("hail")]
+        [Description("Hail Tofu")]
+        [Category(Category.Fun)]
+        public async Task Hail(CommandContext Context)
+        {
+            await Context.RespondAsync(messages[new Random().Next(0, messages.Count)]);
+        }
+
+        public List<string> messages = new List<string>() {
+            "May the blessings of bleating be upon you",
+            "Follow the peaceful path of Tofu",
+            "Where there was only one set of hoofprints, that's when Tofu was carrying you...",
+            "Praise be"
+        };
+    }
+}
+#endif
