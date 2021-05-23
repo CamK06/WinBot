6171e6f95acf98d74012bdc5df3294703a3af8eb
Starman0620 <28871190+Starman0620@users.noreply.github.com>
2021-05-23 02:24:11 -0400

    Added sudo command

---
+++ Source/Commands/Owner/SudoCommand.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+using WinBot.Commands.Attributes;
+
+using WinBot.Misc;
+
+namespace WinBot.Commands.Owner
+{
+    public class SudoCommand : BaseCommandModule
+    {
+        [Command("sudo")]
+        [Description("Execute a command as another user")]
+        [Category(Category.Owner)]
+        public async Task Sudo(CommandContext Context, DiscordUser user, [RemainingText]string command)
+        {
+            // Owner check
+            if(Context.User.Id != Bot.config.ownerId)
+				return;
+			
+            // Find the command
+            Command realCommand = Bot.commands.FindCommand(command, out var args);
+            if(realCommand == null)
+                throw new Exception("Invalid command");
+
+            // Execute the command
+            CommandContext context = Bot.commands.CreateFakeContext(user, Context.Channel, command, ".", realCommand, args);
+			await Bot.commands.ExecuteCommandAsync(context);
+        }
+    }
+}
