0937140047c75f4b1145c7012a5932d42cbaec87
Starman0620 <28871190+Starman0620@users.noreply.github.com>
2021-05-30 15:48:08 -0400

    3.4 - Random bs update

--- Source/Bot.cs
+++ Source/Bot.cs
@@ -22,7 +22,7 @@ namespace WinBot
 {
     class Bot
     {
-        public const string VERSION = "3.3.1";
+        public const string VERSION = "3.4";
 
         static void Main(string[] args) => new Bot().RunBot().GetAwaiter().GetResult();
 
