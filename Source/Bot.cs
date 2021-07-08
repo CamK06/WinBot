9005a29f6400371f79dda0ba29bf9b989c4e4d0e
Starman0620 <28871190+Starman0620@users.noreply.github.com>
2021-07-08 14:25:02 -0400

    V3.4.5 - Fuck you, it's a real thing now

--- Source/Bot.cs
+++ Source/Bot.cs
@@ -22,7 +22,7 @@ namespace WinBot
 {
     class Bot
     {
-        public const string VERSION = "3.4.4";
+        public const string VERSION = "3.4.5";
 
         static void Main(string[] args) => new Bot().RunBot().GetAwaiter().GetResult();
 
