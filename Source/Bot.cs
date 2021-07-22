6eb4ae552d254498b4cfb0d6fd750ab5edee0bbd
Starman0620 <>
2021-07-22 11:20:02 -0400

    Fixed error messages

--- Source/Bot.cs
+++ Source/Bot.cs
@@ -104,7 +104,7 @@ namespace WinBot
                     msg += " This is likely a permissions issue.";
                 
                 await logChannel.SendMessageAsync($"**Command Execution Failed!**\n**Command:** `{e.Command.Name}`\n**Message:** `{e.Context.Message.Content}`\n**Exception:** `{e.Exception}`");
-                await e.Context.RespondAsync($"There was an error executing your command!\nMessage: `{e.Exception.Message}`");
+                await e.Context.RespondAsync($"There was an error executing your command!\nMessage: `{msg}`");
                 
                 // string usage = WinBot.Commands.Main.HelpCommand.GetCommandUsage(e.Command.Name);
                 // if (usage != null)
