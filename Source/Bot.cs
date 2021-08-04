c17938444feede40ab8a2a4625478b6300340b67
Cam K <28871190+Starman0620@users.noreply.github.com>
2021-08-04 00:40:56 -0400

    b

--- Source/Bot.cs
+++ Source/Bot.cs
@@ -199,7 +199,7 @@ namespace WinBot
             }
 #else
             // DONT PING DUFF!
-            if(msg.Content.Contains("283982771997638658") || msg.MentionedUsers.Contains(duff) || msg.ReferencedMessage.Author == duff) {
+            if(msg.Content.Contains("283982771997638658") || msg.MentionedUsers.Contains(duff)) {
                 await msg.Channel.SendMessageAsync("https://tenor.com/view/gordon-ramsay-fuck-off-hells-kitchen-gif-5239890");
             }
 #endif
