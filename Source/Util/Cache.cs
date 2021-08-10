c572b456ad44c2e7c57013e338038a35d094afda
Cam K <28871190+Starman0620@users.noreply.github.com>
2021-08-09 22:45:53 -0400

    Basic cache implementation

---
+++ Source/Util/Cache.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+
+using Serilog;
+using Serilog.Events;
+
+namespace WinBot.Util
+{
+    public class Cache
+    {
+        public static void Verify()
+        {
+            if(!Directory.Exists("Cache")) {
+                Directory.CreateDirectory("Cache");
+                Log.Write(LogEventLevel.Information, "Created new cache directory");
+            }
+        }
+
+        public static void Add(string json, string name)
+        {
+            Cache.Verify();
+            string path = $"Cache/{DateTime.Now.Month}-{DateTime.Now.Day}-{DateTime.Now.Year}_{name}.json";
+            if(File.Exists(path))
+                return;
+
+            File.WriteAllText(path, json);
+        }
+
+        public static string Fetch(string name)
+        {
+            Cache.Verify();
+            string path = $"Cache/{DateTime.Now.Month}-{DateTime.Now.Day}-{DateTime.Now.Year}_{name}.json";
+            if(File.Exists(path))
+                return File.ReadAllText(path);
+            else {
+                Log.Write(LogEventLevel.Warning, $"{name} does not exist in the cache, returning null.");
+                return null;
+            }
+        }
+    }
+}
