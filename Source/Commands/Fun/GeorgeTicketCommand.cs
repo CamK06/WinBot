d303af882af7f0a1cf734a6ce98651d243f148b1
Cam K <28871190+Starman0620@users.noreply.github.com>
2021-08-12 13:42:44 -0400

    Fix embed colour in ticket command

--- Source/Commands/Fun/GeorgeTicketCommand.cs
+++ Source/Commands/Fun/GeorgeTicketCommand.cs
@@ -40,6 +40,7 @@ namespace WinBot.Commands.Fun
             eb.WithTitle("Random George Ticket");
             eb.WithImageUrl(ticketUrl);
             eb.WithFooter($"ID: {ticketID}");
+	    eb.WithColor(DiscordColor.Gold);
             eb.WithUrl("https://www.chroniclesofgeorge.com/");
             await Context.RespondAsync("", eb.Build());
         }
