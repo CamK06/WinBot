e27b7bb4841b27273df7cbdc727f54c1468f27a7
Cam K <28871190+Starman0620@users.noreply.github.com>
2021-08-12 00:20:47 -0400

    Enhance bed command

--- Source/Commands/Fun/BedCommand.cs
+++ Source/Commands/Fun/BedCommand.cs
@@ -15,22 +15,49 @@ namespace WinBot.Commands.Fun
     {
 		[Command("bed")]
         [Description("Tell someone to go to bed.")]
-        [Usage("[Name of person you want to go to bed]")]
+        [Usage("[User] [Image: parz, agp, agp2, mehdi]")]
         [Category(Category.Fun)]
-        public async Task bed(CommandContext Context, string screenname = "")
+        public async Task bed(CommandContext Context, string screenname = "", string image = "parz")
         {
-			
+			// Select the image with some YanDev code
+            string imageFile = "parz.png";
+            float genX = 365f;
+            float bedY = 506f;
+            float userY = 615f;
+            int fontSize = 70;
+            if(image.ToLower() == "agp") {
+                imageFile = "agp.png";
+                genX = 125;
+                bedY = 35;
+                fontSize = 50;
+                userY = 360;
+            }
+            else if(image.ToLower() == "agp2") {
+                imageFile = "agp2.png";
+                genX = 177.5f;
+                bedY = 60;
+                fontSize = 50;
+                userY = 450;
+            }
+            else if(image.ToLower() == "mehdi") {
+                imageFile = "mehdi.png";
+                bedY = 100;
+                genX = 307.5f; 
+                userY = 680f;
+            }
+
+            // Load the font
             PrivateFontCollection fonts = new PrivateFontCollection();
             fonts.AddFontFile("impact.ttf");
 			
 			// Create the image
-            Bitmap parz = new Bitmap(Bitmap.FromFile("parz.png"));
-			Graphics bmp = Graphics.FromImage(parz);
+            Bitmap img = new Bitmap(Bitmap.FromFile(imageFile));
+			Graphics bmp = Graphics.FromImage(img);
     
 			 // Set up the fonts and drawing stuff
             Font IMPACTfont = new Font(
                 fonts.Families[0].Name,
-                70,
+                fontSize,
                 FontStyle.Regular,
                 GraphicsUnit.Pixel
             );
@@ -42,12 +69,12 @@ namespace WinBot.Commands.Fun
 			snFormat.LineAlignment = StringAlignment.Center;
 			
 			// Draw the text onto the image
-            bmp.DrawString("GO TO BED", IMPACTfont, brush, 227.0F, 506.0f);
-			bmp.DrawString(screenname.ToUpper(), IMPACTfont, brush, 365.0F, 615.0f, snFormat);
+            bmp.DrawString("GO TO BED", IMPACTfont, brush, genX, bedY, snFormat);
+			bmp.DrawString(screenname.ToUpper(), IMPACTfont, brush, genX, userY, snFormat);
 			
 			// Save the image to a temporary file
             bmp.Save();
-            parz.Save("bed.png");
+            img.Save("bed.png");
 
             await Context.Channel.SendFileAsync("bed.png");
 		}
