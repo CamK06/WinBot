ca148ac36e5f25300f2e3e0af60c6468a7867a36
Starman0620 <>
2021-08-20 13:02:44 -0400

    Await dm echoing in Tofu

--- Source/Misc/DMSystem.cs
+++ Source/Misc/DMSystem.cs
@@ -55,9 +55,9 @@ namespace WinBot
                 }
 #if TOFU
                 else if(args.Channel.IsPrivate && !args.Author.IsBot) {
-                    Bot.staffChannel.SendMessageAsync("DM From " + args.Author.Username + ": " + args.Message.Content.Replace("@", "(at)"));
+                    await Bot.staffChannel.SendMessageAsync("DM From " + args.Author.Username + ": " + args.Message.Content.Replace("@", "(at)"));
                     if(args.Message.Attachments.FirstOrDefault() != null) {
-                        Bot.staffChannel.SendMessageAsync(args.Message.Attachments.FirstOrDefault().Url);
+                        await Bot.staffChannel.SendMessageAsync(args.Message.Attachments.FirstOrDefault().Url);
                     }
                 }
 #endif
