eca94baf5d059691c8adf28a7ac2bad8b63c5694
Starman0620 <>
2021-08-25 01:31:37 -0400

    Fix mute role finding in Tofu

--- Source/Bot.cs
+++ Source/Bot.cs
@@ -154,7 +154,7 @@ namespace WinBot
                 }
                 
 #if TOFU
-                mutedRole = client.Guilds.FirstOrDefault().Value.Roles.FirstOrDefault(x => x.Value.Name.ToLower() == "muted").Value;
+                mutedRole = client.Guilds.FirstOrDefault().Value.GetRole(874370290900140084); // TODO: make role ID not hardcoded
                 welcomeChannel = await client.GetChannelAsync(config.welcomeChannel);
                 staffChannel = await client.GetChannelAsync(config.staffChannel);
                 HauntSystem.Init();
