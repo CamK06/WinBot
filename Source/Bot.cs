e23dd43a8fd8a8224fb8ce4041862988ba8bacab
Starman0620 <>
2021-08-25 01:49:30 -0400

    Update version string

--- Source/Bot.cs
+++ Source/Bot.cs
@@ -25,7 +25,7 @@ namespace WinBot
 {
     class Bot
     {
-        public const string VERSION = "3.6.1";
+        public const string VERSION = "3.6.2";
 
         static void Main(string[] args) => new Bot().RunBot().GetAwaiter().GetResult();
 
