5c93a574aaf8ac8d032677fa838bda112c668f41
Cam K <N/A>
2021-12-12 04:40:24 -0500

    Add react purge

---
+++ Source/Commands/Owner/ReactPurgeCommand.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+using WinBot.Commands.Attributes;
+
+
+namespace WinBot.Commands.Staff
+{
+    public class ReactPurgeCommand : BaseCommandModule
+    {
+        [Command("purge")]
+        [Category(Category.Staff)]
+        [RequireUserPermissions(DSharpPlus.Permissions.ManageMessages)]
+        public async Task ReactPurge(CommandContext Context, int count = 0)
+        {
+            if(Context.User.Id != Bot.config.ownerId && !Bot.whitelistedUsers.Contains(Context.User.Id))
+				throw new Exception("You must be the bot owner to run this command!");
+
+            await Context.Message.DeleteAsync();
+
+            // Delete the messages
+            var messages = await Context.Channel.GetMessagesAsync(count+1);
+            foreach(var message in messages) {
+                await message.DeleteAllReactionsAsync();
+                await Task.Delay(70);
+            }
+        }
+    }
+}
