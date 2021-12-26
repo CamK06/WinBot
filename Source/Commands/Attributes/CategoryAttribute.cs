837d6b658c162bc174a78fc8a28294abc762453a
Cam K <N/A>
2021-12-26 00:40:46 -0500

    Add help command

---
+++ Source/Commands/Attributes/CategoryAttribute.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace WinBot.Commands.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Parameter, AllowMultiple = false)]
+    public sealed class CategoryAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the category of this command
+        /// </summary>
+        public Category category { get; }
+
+        public CategoryAttribute(Category category)
+        {
+            this.category = category;
+        }
+    }
+
+    public enum Category
+    {
+        Main, Fun, Misc, Staff, Owner
+    }
+}
--- Source/Commands/Main/PingCommand.cs
+++ Source/Commands/Main/PingCommand.cs
@@ -3,16 +3,18 @@ using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 
+using WinBot.Commands.Attributes;
+
 namespace WinBot.Commands.Main
 {
     public class PingCommand : BaseCommandModule
     {
         [Command("ping")]
         [Description("Gets the bots latency to Discord")]
-        //[Category(Category.Main)]
+        [Category(Category.Main)]
         public async Task Ping(CommandContext Context)
         {
-            await Context.ReplyAsync($"🏓 Pong! **{Bot.client.Ping}ms** from WinBot {Bot.VERSION}");
+            await Context.ReplyAsync($"🏓 Pong! **{Bot.client.Ping}ms**");
         }
     }
 }
