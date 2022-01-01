10d3d3ce0daa567955f1f7e4e75b9e82127b3375
Cam K <N/A>
2021-12-31 23:21:44 -0500

    Add wiki command

---
+++ Source/Commands/Main/WikiCommand.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using System.Threading.Tasks;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+using WinBot.Commands.Attributes;
+
+using WikipediaNet;
+using WikipediaNet.Objects;
+
+namespace WinBot.Commands.Main
+{
+    public class WikiCommand : BaseCommandModule
+    {
+        [Command("wiki")]
+        [Description("Search wikipedia")]
+        [Usage("[query]")]
+        [Category(Category.Main)]
+        public async Task Wiki(CommandContext Context, [RemainingText] string query)
+        {
+            if(string.IsNullOrWhiteSpace(query))
+                throw new System.Exception("You must provide a search query!");
+
+            Wikipedia wiki = new Wikipedia();
+            wiki.Limit = 1;
+            QueryResult results = wiki.Search(query);
+
+            if (results.SearchInfo.TotalHits < 1) {
+                await Context.ReplyAsync($"Error: There are no results for that query.");
+                return;
+            }
+
+            await Context.ReplyAsync(results.Search.First().Url.ToString().Replace(" ", "_"));
+        }
+    }
+}
