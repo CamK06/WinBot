a706e3aa8e29d790c4fcd78d17469bd7d40a5507
Cam K <a@a>
2022-01-06 23:01:47 -0500

    h

--- Source/Util/ImageCommandParser.cs
+++ Source/Util/ImageCommandParser.cs
@@ -109,8 +109,8 @@ namespace WinBot.Util
             Int64 fileSize = Convert.ToInt64(client.ResponseHeaders["Content-Length"]);
             if (!client.ResponseHeaders["Content-Type"].Contains("image") || client.ResponseHeaders["Content-Type"].Contains("svg"))
                 throw new Exception("Invalid or no image!");
-            if(fileSize > 16777216)
-                throw new Exception("Your image must be below 16MB in size!");
+            if(fileSize > 33554432)
+                throw new Exception("Your image must be below 32MB in size!");
             args.extension = client.ResponseHeaders["Content-Type"].Split("image/").Last();
 
             return args;
