5ef7e46d84d48798a47a104581e5ae40400c5d78
Cam K <a@a>
2022-01-18 15:36:14 -0500

    More trivia things

--- Source/Commands/Fun/TriviaCommand.cs
+++ Source/Commands/Fun/TriviaCommand.cs
@@ -36,7 +36,7 @@ namespace WinBot.Commands.Main
             }
             else if(input != null && input.ToLower() == "lb") {
                 
-                List<User> leaderboard = UserData.users.OrderByDescending(x => (x.correctTrivia/x.totalTrivia)*x.correctTrivia).ToList();
+                List<User> leaderboard = UserData.users.OrderByDescending(x => (x.correctTrivia <= 0 || x.totalTrivia <= 0) ? x.correctTrivia : (x.correctTrivia/x.totalTrivia)*x.correctTrivia).ToList();
 
                 // Generate an embed description
                 string description = "";
