1006b404bac5777d66bd31a10260c2e7bd689454
Cam K <a@a>
2022-05-10 15:15:27 -0400

    Fix whois

--- Source/Commands/Main/WhoisCommand.cs
+++ Source/Commands/Main/WhoisCommand.cs
@@ -18,6 +18,8 @@ namespace WinBot.Commands.Main
         [Category(Category.Main)]
         public async Task Whois(CommandContext Context, [RemainingText] DiscordUser user)
         {
+            if(user == null)
+                user = Context.User;
             DiscordMember memberUser = user as DiscordMember;
 
             try {
