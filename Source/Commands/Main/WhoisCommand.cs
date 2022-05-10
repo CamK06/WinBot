2f70a878d234c91490a33914c7024fb68e5e9df9
Cam K <a@a>
2022-05-10 15:01:32 -0400

    Update whois

--- Source/Commands/Main/WhoisCommand.cs
+++ Source/Commands/Main/WhoisCommand.cs
@@ -16,10 +16,9 @@ namespace WinBot.Commands.Main
         [Description("Gets basic info about a user")]
         [Usage("[user]")]
         [Category(Category.Main)]
-        public async Task Whois(CommandContext Context, [RemainingText] DiscordMember user)
+        public async Task Whois(CommandContext Context, [RemainingText] DiscordUser user)
         {
-            if (user == null) 
-                user = Context.Message.Author as DiscordMember;
+            DiscordMember memberUser = user as DiscordMember;
 
             try {
                 // Set up the embed
@@ -39,7 +38,8 @@ namespace WinBot.Commands.Main
 
                 // Embed dates
                 Embed.AddField("**Created On**", $"{MiscUtil.FormatDate(user.CreationTimestamp)} ({(int)DateTime.Now.Subtract(user.CreationTimestamp.DateTime).TotalDays} days ago)", true);
-                Embed.AddField("**Joined On**", $"{MiscUtil.FormatDate(user.JoinedAt.DateTime)} ({(int)DateTime.Now.Subtract(user.JoinedAt.DateTime).TotalDays} days ago)", true);
+                if(memberUser != null)
+                    Embed.AddField("**Joined On**", $"{MiscUtil.FormatDate(memberUser.JoinedAt.DateTime)} ({(int)DateTime.Now.Subtract(memberUser.JoinedAt.DateTime).TotalDays} days ago)", true);
 
                 await Context.ReplyAsync("", Embed.Build());
             }
