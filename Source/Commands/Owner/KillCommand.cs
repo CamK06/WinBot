016f1b77fef422f1d9eaf62464bb2beac6562faf
Cam K <a@a>
2023-02-11 22:24:20 -0500

    whath

--- Source/Commands/Owner/KillCommand.cs
+++ Source/Commands/Owner/KillCommand.cs
@@ -20,12 +20,31 @@ namespace WinBot.Commands.Owner
         [RequireOwner]
         public async Task Kill(CommandContext Context)
         {
-            await Context.ReplyAsync("https://tenor.com/bbEm5.gif");
-			await Context.ReplyAsync("Shutting down...");
+            await Context.ReplyAsync(replyGifs[new Random().Next(0, replyGifs.Length)]);
 			Log.Information("Shutdown triggered by command");
 			//DailyReportSystem.CreateBackup();
             UserData.SaveData();
 			Environment.Exit(0);
         }
+
+        static string[] replyGifs = {
+            "https://tenor.com/bbEm5.gif",
+            "https://tenor.com/Z6a1.gif",
+            "https://tenor.com/bjJy4.gif",
+            "https://tenor.com/KC31.gif",
+            "https://tenor.com/bBZrD.gif",
+            "https://tenor.com/bkhTF.gif",
+            "https://tenor.com/bM2j7.gif",
+            "https://tenor.com/bckT6.gif",
+            "https://tenor.com/t8rI.gif",
+            "https://tenor.com/t8qR.gif",
+            "https://tenor.com/Tm09.gif",
+            "https://tenor.com/bzcn5.gif",
+            "https://tenor.com/bPRLn.gif",
+            "https://tenor.com/8Hw2.gif",
+            "https://tenor.com/biuHb.gif",
+            "https://tenor.com/biC9R.gif",
+            "https://tenor.com/bVtZn.gif"
+        };
     }
 }
