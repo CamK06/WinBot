6c3699256b22acf0ecd531a61b38f173097716c4
Cam K <a@a>
2023-02-25 19:26:09 -0500

    do a thing

--- Source/Misc/WWRSS.cs
+++ Source/Misc/WWRSS.cs
@@ -68,7 +68,8 @@ namespace WinBot.Misc
                 if(!(item.Title.ToLower().Contains("asteroid") || item.Title.ToLower().Contains("earth")
                 || item.Title.ToLower().Contains("nasa said") || item.Title.ToLower().Contains("nasa says")
                 || item.Title.ToLower().Contains("nasa warns") || item.Title.ToLower().Contains("says nasa")
-		|| item.Title.ToLower().Contains("alien")))
+		        || item.Title.ToLower().Contains("alien") || item.Title.ToLower().Contains("ufo")
+                || item.Title.ToLower().Contains("mars")))
                     continue;
 #endif
 
